403a185b4008640adf14df63524d69955fb6cb95
Lenovo <anton.semerenko@gmail.com>
2025-01-15 21:29:04 +0200

    HW2.1 Anton Semerenko

--- HW2/HW2.1.py
+++ HW2/HW2.1.py
@@ -1,18 +1,17 @@
+x = 1 - 9
+x1 = x
+x2 = x1
+x3 = x2
 user_input = input("Please enter any 4-digit integer.: from 1 to 9")
 number = int(user_input)
-print(number / 2)
-x=1-9
-x1=x
-x2=x1
-x3=x2
-
-
-
+print(type(number))
+print(divmod(x+x1+x2+x3))
 
 import sys
-print('Hello world')
-print(sys.argv[0])
-print(sys.argv[1])
+print('x')
+print(sys.argv[x1])
+print(sys.argv[x2])
+print(sys.argv[x3])
 
 
 
