61e35b919fc303bde6ac8e2fd9f71a5c92ee5350
Lenovo <anton.semerenko@gmail.com>
2025-01-15 21:13:40 +0200

    start commit

---
+++ HW2/HW2.1.py
@@ -0,0 +1,18 @@
+user_input = input("Please enter any 4-digit integer.: from 1 to 9")
+number = int(user_input)
+print(number / 2)
+x=1-9
+x1=x
+x2=x1
+x3=x2
+
+
+
+
+import sys
+print('Hello world')
+print(sys.argv[0])
+print(sys.argv[1])
+
+
+
