e12f300f12c195d416971e731ee80f947b4dc8e0
Lenovo <anton.semerenko@gmail.com>
2025-01-26 22:46:35 +0200

    HW3.1 finished2

--- HW3/HW3.1.py
+++ HW3/HW3.1.py
@@ -2,11 +2,9 @@ n1=int(input("Enter 1st number, please  \n"))
 n2=int(input("Enter 2nd number, please  \n"))
 a=input("Enter action, please  \n")
 if a=="/":
-    print('Result:')
-    print(n1/n2)
     if 0 < n2:
         print('Result:')
-        print(n1 / n2)
+        print(n1/n2)
     else:
         print('division by zero')
 elif a=="*":
@@ -18,4 +16,3 @@ elif a=="+":
 elif a == "-":
     print('Result:')
     print(n1-n2)
-
