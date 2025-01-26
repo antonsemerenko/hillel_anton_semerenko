ee4c03c123e822641aafe610e46f8ccfe50f9fed
Lenovo <anton.semerenko@gmail.com>
2025-01-26 21:40:47 +0200

    HW3.1 finished

--- HW3/HW3.1.py
+++ HW3/HW3.1.py
@@ -1,1 +1,15 @@
-user_input = input("Please enter any 4-digit integer: ")
+n1=int(input("Enter 1st number, please  \n"))
+n2=int(input("Enter 2nd number, please  \n"))
+a=input("Enter action, please  \n")
+if a=="/":
+    print('Result:')
+    print(n1/n2)
+elif a=="*":
+    print('Result:')
+    print(n1*n2)
+elif a=="+":
+    print('Result:')
+    print(n1+n2)
+elif a == "-":
+    print('Result:')
+    print(n1-n2)
