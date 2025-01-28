6615d38385cba7f54c5dd723a1275792d59804a5
Lenovo <anton.semerenko@gmail.com>
2025-01-29 00:01:10 +0200

    HW3.1 finished

--- HW3/HW3.2.py
+++ HW3/HW3.2.py
@@ -1,10 +1,9 @@
-# lst=[12, 3, 4, 10] #=> [10, 12, 3, 4]
-# lst=[1] #=> [1]
-# lst=[] #=> []
-# lst=[12, 3, 4, 10, 8] #=> [8, 12, 3, 4, 10]
+#lst=[12, 3, 4, 10] #=> [10, 12, 3, 4]
+#lst=[1] #=> [1]
+#lst=[] #=> []
+#lst=[12, 3, 4, 10, 8] #=> [8, 12, 3, 4, 10]
 
-lst=1
-
-print("Hello World")
-print("Hello World")
-print("Hello World")
+lst1=[lst[-1]]
+lst.pop()
+lst1+=lst
+print(lst1)
