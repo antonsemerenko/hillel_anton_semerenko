c670704c2c64d084a3ce0e0c30d5e5a0f56f4473
Lenovo <anton.semerenko@gmail.com>
2025-01-29 22:07:54 +0200

    HW3.1 finished

--- HW3/HW3.2.py
+++ HW3/HW3.2.py
@@ -1,10 +1,14 @@
 #lst=[12, 3, 4, 10] #=> [10, 12, 3, 4]
 #lst=[1] #=> [1]
-lst=[] #=> []
-#lst=[12, 3, 4, 10, 8] #=> [8, 12, 3, 4, 10]
+#lst = []  #=> []
+lst=[12, 3, 4, 10, 8] #=> [8, 12, 3, 4, 10]
 
+if [] not in lst:
+    print(lst)
+else:
+    lst = [0]
+    lst1 = [lst[-1]]
+    lst.pop()
+    lst1 += lst
+    print(lst1)
 
-lst1=[lst[-1]]
-lst.pop()
-lst1+=lst
-print(lst1)
